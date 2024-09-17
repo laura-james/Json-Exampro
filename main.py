d8d22df6b80b21a3bb38fc217f3c1b2dd6549d88
Laura James <lollypopstar70@gmail.com>
2024-09-17 16:53:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import json
+
+with open('exampro.json') as f:
+    d = json.load(f)
+myfile = open("output.txt","a")
+for doc in d:
+    print("https://"+doc["docid"]+".exampro.net", end= " ")
+    print(doc["documentname"], end= " ")
+    print(doc["author"], end= " ")
+    print(doc["datesaved"], end= " ")
+    print(doc["questioncount"])
+    msg = doc["documentname"]+"*"+doc["author"]+"*"+doc["datesaved"]+"*"+str(doc["questioncount"])+"*https://"+doc["docid"]+".exampro.net"
+    myfile.write(msg+"\n")
+myfile.close()
